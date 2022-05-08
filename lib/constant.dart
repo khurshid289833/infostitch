b2ed3f3618bc785aded636ccfc5d38740e35a829
technoexponent <khurshid@technoexponent.com>
2022-05-08 19:28:05 +0530

    final push

--- lib/constant.dart
+++ lib/constant.dart
@@ -11,6 +11,8 @@ const  grey2 = Color.fromRGBO(255, 255, 255, 0.6);
 const  purpleColor = Color.fromRGBO(187, 134, 252, 1);
 const  purpleBackground = Color.fromRGBO(58, 53, 63, 1);
 
+
+
 Widget cross(height,width) =>  Align(
   alignment: Alignment.topRight,
   child: Padding(
